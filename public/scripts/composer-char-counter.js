001d155fe651b9eb29fe73497e329e89910a92b2
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-21 20:56:50 +0000

    makes counter red when out of characters and refacters code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 
   $('textarea').on('keyup', function(event) {
     var charset = (140 - event.target.value.length);
-    $(event.target).parent().children('.counter').text(charset);
-  })
-})
+    var counter = $(event.target).parent().children('.counter');
+
+    counter.text(charset);
+
+    if (charset < 0) {
+      counter.addClass('negCharset');
+    } else {
+      counter.removeClass('negCharset');
+    }
+  });
+});
